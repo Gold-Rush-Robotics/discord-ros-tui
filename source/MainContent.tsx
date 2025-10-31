96b856496358bd667c27278f17068edf831cadf2
Hudson111-s <145516182+Hudson111-s@users.noreply.github.com>
2025-10-31 16:58:30 -0400

    Add general layout

--- source/MainContent.tsx
+++ source/MainContent.tsx
@@ -3,12 +3,35 @@ import React from 'react';
 import {MessageInput} from './MessageInput.js';
 
 export function MainContent({status}: {status: string}) {
-	return (
-		<Box flexDirection="column" flexGrow={1}>
-			<Box borderStyle="single" flexGrow={1}>
-				<Text>MAIN CONTENT</Text>
-			</Box>
-			<MessageInput input={status} />
-		</Box>
-	);
+  return (
+    <>
+      <Box flexDirection="column" flexGrow={1}>
+        <Box flexDirection="row" flexGrow={1}>
+          <Box borderStyle="single" flexDirection="column" width={25}>
+
+            <Box flexDirection="column" flexGrow={1} paddingX={1}>
+              <Text bold>Nodes</Text>
+              <Text>Node1</Text>
+              <Text>Node2</Text>
+            </Box>
+
+            <Box flexDirection="column" flexGrow={1} paddingX={1}>
+              <Text bold>Topics</Text>
+              <Text>Topic1</Text>
+              <Text>Topic2</Text>
+            </Box>
+
+          </Box>
+
+          <Box borderStyle="single" flexDirection="column" flexGrow={1} paddingX={1}>
+            <Text bold>Main content</Text>
+            <Text>Show info here</Text>
+          </Box>
+
+        </Box>
+
+        <MessageInput input={status} />
+      </Box>
+    </>
+  );
 }
