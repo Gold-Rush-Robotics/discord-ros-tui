8f4af6f49c91667afbbaae005a20edcd50655e62
Justin Brooks <jnbrooks457@gmail.com>
2025-10-31 19:09:18 -0400

    Minor view tweaks (border/padding)

--- source/MainContent.tsx
+++ source/MainContent.tsx
@@ -3,35 +3,47 @@ import React from 'react';
 import {MessageInput} from './MessageInput.js';
 
 export function MainContent({status}: {status: string}) {
-  return (
-    <>
-      <Box flexDirection="column" flexGrow={1}>
-        <Box flexDirection="row" flexGrow={1}>
-          <Box borderStyle="single" flexDirection="column" width={25}>
+	return (
+		<>
+			<Box flexDirection="column" flexGrow={1}>
+				<Box flexDirection="row" flexGrow={1} gap={1}>
+					<Box flexDirection="column" width={25}>
+						<Box
+							borderStyle="single"
+							flexDirection="column"
+							flexGrow={1}
+							paddingX={1}
+						>
+							<Text bold>Nodes</Text>
+							<Text>Node1</Text>
+							<Text>Node2</Text>
+						</Box>
 
-            <Box flexDirection="column" flexGrow={1} paddingX={1}>
-              <Text bold>Nodes</Text>
-              <Text>Node1</Text>
-              <Text>Node2</Text>
-            </Box>
+						<Box
+							borderStyle="single"
+							flexDirection="column"
+							flexGrow={1}
+							paddingX={1}
+						>
+							<Text bold>Topics</Text>
+							<Text>Topic1</Text>
+							<Text>Topic2</Text>
+						</Box>
+					</Box>
 
-            <Box flexDirection="column" flexGrow={1} paddingX={1}>
-              <Text bold>Topics</Text>
-              <Text>Topic1</Text>
-              <Text>Topic2</Text>
-            </Box>
+					<Box
+						borderStyle="single"
+						flexDirection="column"
+						flexGrow={1}
+						paddingX={1}
+					>
+						<Text bold>Main content</Text>
+						<Text>Show info here</Text>
+					</Box>
+				</Box>
 
-          </Box>
-
-          <Box borderStyle="single" flexDirection="column" flexGrow={1} paddingX={1}>
-            <Text bold>Main content</Text>
-            <Text>Show info here</Text>
-          </Box>
-
-        </Box>
-
-        <MessageInput input={status} />
-      </Box>
-    </>
-  );
+				<MessageInput input={status} />
+			</Box>
+		</>
+	);
 }
