a77eb5ee0d446d0400c9f6577553c7e0175fb499
Hudson111-s <145516182+Hudson111-s@users.noreply.github.com>
2025-11-02 20:25:20 -0500

    Add scrolling to SelectableList

--- source/MainContent.tsx
+++ source/MainContent.tsx
@@ -74,9 +74,9 @@ export function MainContent({ status }: { status: string }) {
             <Box
               borderStyle="single"
               flexDirection="column"
-              flexGrow={1}
               paddingX={1}
               overflow="hidden"
+              height={"50%"}
             >
               <Topics interactable={true} />
             </Box>
@@ -84,9 +84,9 @@ export function MainContent({ status }: { status: string }) {
             <Box
               borderStyle="single"
               flexDirection="column"
-              flexGrow={1}
               paddingX={1}
               overflow="hidden"
+              height={"50%"}
             >
               {carousel[carouselIndex]?.element}
             </Box>
